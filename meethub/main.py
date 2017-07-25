bab7e934091dcd1dc2945dc2fa0c97e465776117
Cris546 <theonlyjeff223@gmail.com>
2017-07-25 11:41:15 -0700

    Still username login

--- meethub/main.py
+++ meethub/main.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 import jinja2
 import user
@@ -36,6 +21,7 @@ class MainPage(webapp2.RequestHandler):
 
         self.response.write('<html><body>%s</body></html>' % greeting)
 
+        
 
 class SignInHandler(webapp2.RequestHandler):
     def get(self):
@@ -46,8 +32,7 @@ class SignInHandler(webapp2.RequestHandler):
 # class SignUpHandler(webapp2.RequestHandler):
 #     def get(self):
 
-class CreatePost(webapp2.RequestHandler):
-    def get(self):
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
