d9928cdbf17c398dc5af59db76af7bf2b1afcd07
Demo User <demouser@MacBook-Air.local>
2017-07-25 11:10:21 -0700

    Nick Update

--- meethub/templates/mainhub.js
+++ meethub/templates/mainhub.js
@@ -1,1 +1,10 @@
 console.log("Yo");
+function write_user_name () {
+  var user_name = $("#name_box").val();
+  $("#posts").text(user_name);
+}
+function setUp() {
+  $("#button_ok").click(write_user_name);
+}
+
+$(document).ready(setUp);
